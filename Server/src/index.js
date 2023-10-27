c740918701dd76d847f8ce48cb5ee593e126ede2
PedroCavataio <pedrocavataio@gmail.com>
2023-10-27 18:50:50 -0300

    prueba local en thunder

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,5 +1,5 @@
 const server = require("./app.js");
-const PORT = process.env.PORT/*  || 3001 */;
+const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
    console.log('Server raised in port: ' + PORT);
