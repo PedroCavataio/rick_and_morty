14aedd5ac855302be087fe60074fe39df7863ea1
PedroCavataio <pedrocavataio@gmail.com>
2023-10-28 11:23:41 -0300

    probando variables de entorno

--- Client/src/index.js
+++ Client/src/index.js
@@ -11,7 +11,7 @@
 
   ReactDOM.render(
     <React.StrictMode>
-      <BrowserRouter basename='/rickandmorty'>
+      <BrowserRouter>
       <Provider store={store}>
         <App />
         </Provider>
