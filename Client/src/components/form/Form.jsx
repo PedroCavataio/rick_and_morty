64582c5d4260c1cbad7836c9743985c384add74e
PedroCavataio <pedrocavataio@gmail.com>
2024-01-08 01:20:33 +0100

    cambio en string form

--- Client/src/components/form/Form.jsx
+++ Client/src/components/form/Form.jsx
@@ -58,7 +58,7 @@ const Form = ({ onLogin, access }) => {
   return (
     <form className={styles["form-container"]} onSubmit={handleSubmit}>
       <div className="form-group">
-        <label htmlFor="email">Email: (ejemplo@gmail.com)</label>
+        <label htmlFor="email">Escribe el siguiente Email: ejemplo@gmail.com</label>
         <input
           type="email"
           id="email"
@@ -72,7 +72,7 @@ const Form = ({ onLogin, access }) => {
         {errors.email && <span className={styles["error-message"]}>{errors.email}</span>}
       </div>
       <div className="form-group">
-        <label htmlFor="password">Password: (123456)</label>
+        <label htmlFor="password">Escribe la siguiente Password: 123456</label>
         <input
           type="password"
           id="password"
